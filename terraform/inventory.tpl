5f81cc0b65ecfedbe3453805c37a36d9105c371f
Julio Colon <jco2344@wgu.edu>
2025-11-09 03:07:15 -0500

    changed to use tpl

---
+++ terraform/inventory.tpl
@@ -0,0 +1,11 @@
+[controller]
+${controller_name} ansible_host=${controller_public_ip} private_ip=${controller_private_ip}
+
+[compute]
+%{ for instance in compute_instances ~}
+${instance.tags.Name} ansible_host=${instance.public_ip} private_ip=${instance.private_ip}
+%{ endfor ~}
+
+[cluster:children]
+controller
+compute
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,15 @@
 # Creates the anisible inventoy file and populates it with
 resource "local_file" "ansible_inventory" {
-  content = <<-EOT
-# generated ${timestamp()}
-
-[controller]
-${aws_instance.controller.tags.Name} ansible_host=${aws_instance.controller.public_ip} private_ip=${aws_instance.controller.private_ip}
-
-[compute]
-%{ for i, instance in aws_instance.compute }
-${instance.tags.Name} ansible_host=${instance.public_ip} private_ip=${instance.private_ip}
-%{ endfor ~}
-
-[cluster:children]
-controller
-compute
-EOT
-
   filename = "${path.module}/../ansible/inventory.ini"
+
+  content = templatefile("${path.module}/inventory.tpl", {
+    # Pass variables from our Terraform resources into the template
+    timestamp            = timestamp()
+    controller_name      = aws_instance.controller.tags.Name
+    controller_public_ip = aws_instance.controller.public_ip
+    controller_private_ip= aws_instance.controller.private_ip
+    compute_instances    = aws_instance.compute
+  })
 }
 
 output "controller_public_ip" {
